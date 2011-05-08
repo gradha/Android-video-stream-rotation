337206c4f024abeae16f2ee251b8e07e73ce0793
Grzegorz Adam Hankiewicz <gradha@titanium.sabren.com>
2011-05-08 19:58:56 +0200

    Starting from template.

---
+++ src/com/elhaso/tutinterruptvideostream/HelloInterruptVideoStream.java
@@ -0,0 +1,13 @@
+package com.elhaso.tutinterruptvideostream;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloInterruptVideoStream extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
