8a299d2c277811948ef29811595583f950477287
Grzegorz Adam Hankiewicz <gradha@titanium.sabren.com>
2011-05-08 20:11:52 +0200

    Total fail from the easy videoview.

--- src/com/elhaso/tutinterruptvideostream/HelloInterruptVideoStream.java
+++ src/com/elhaso/tutinterruptvideostream/HelloInterruptVideoStream.java
@@ -1,13 +1,26 @@
 package com.elhaso.tutinterruptvideostream;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-public class HelloInterruptVideoStream extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class HelloInterruptVideoStream extends Activity
+{
+	private String path = "http://dl.dropbox.com/u/145894/t/rabbits.3gp";
+	private VideoView video_view_;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		video_view_ = (VideoView)findViewById(R.id.surface_view);
+
+		video_view_.setVideoURI(Uri.parse(path));
+		video_view_.setMediaController(new MediaController(this));
+		video_view_.requestFocus();
+	}
 }
