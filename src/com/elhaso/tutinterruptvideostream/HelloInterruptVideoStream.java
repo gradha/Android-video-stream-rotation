bae6b602260092417f43732f7cf3d121c2c085bc
Grzegorz Adam Hankiewicz <gradha@titanium.sabren.com>
2011-05-10 10:17:41 +0200

    Working interruption rotation, bad screen size.

--- src/com/elhaso/tutinterruptvideostream/HelloInterruptVideoStream.java
+++ src/com/elhaso/tutinterruptvideostream/HelloInterruptVideoStream.java
@@ -1,15 +1,16 @@
 package com.elhaso.tutinterruptvideostream;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.MediaController;
-import android.widget.VideoView;
 
 public class HelloInterruptVideoStream extends Activity
 {
 	private String path = "http://dl.dropbox.com/u/145894/t/rabbits.3gp";
-	private VideoView video_view_;
+	private VideoViewCustom video_view_;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -17,10 +18,39 @@ public class HelloInterruptVideoStream extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		video_view_ = (VideoView)findViewById(R.id.surface_view);
+		video_view_ = (VideoViewCustom)findViewById(R.id.surface_view);
+		video_view_.setDimensions(200, 200);
+		video_view_.getHolder().setFixedSize(200, 200);
 
 		video_view_.setVideoURI(Uri.parse(path));
 		video_view_.setMediaController(new MediaController(this));
 		video_view_.requestFocus();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig)
+	{
+		super.onConfigurationChanged(newConfig);
+
+		if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+			/*
+			getWindow().clearFlags(WindowManager
+				.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+			*/
+
+			video_view_.setDimensions(200, 200);
+			video_view_.getHolder().setFixedSize(200, 200);
+		} else {
+			/*
+			getWindow().setFlags(
+				WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+				*/
+
+			video_view_.setDimensions(200, 200);
+			video_view_.getHolder().setFixedSize(200, 200);
+		}
 	}
 }
---
+++ src/com/elhaso/tutinterruptvideostream/VideoViewCustom.java
@@ -0,0 +1,44 @@
+package com.elhaso.tutinterruptvideostream;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.VideoView;
+
+/** Code from
+ * http://stackoverflow.com/questions/4434027/android-videoview-orientation-change-with-buffered-video
+ */
+
+public class VideoViewCustom extends VideoView
+{
+	private int mForceHeight = 0;
+	private int mForceWidth = 0;
+
+	public VideoViewCustom(Context context)
+	{
+		super(context);
+	}
+
+	public VideoViewCustom(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+
+	public VideoViewCustom(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+	}
+
+	public void setDimensions(int w, int h)
+	{
+		this.mForceHeight = h;
+		this.mForceWidth = w;
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+	{
+		Log.i("VideoViewCustom", "onMeasure");
+		setMeasuredDimension(mForceWidth, mForceHeight);
+	}
+}
