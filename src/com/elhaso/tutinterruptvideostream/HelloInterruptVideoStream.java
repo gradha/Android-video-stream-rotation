84d9d6435749fdbb6af5bdae2d3b09928d44704b
Grzegorz Adam Hankiewicz <gradha@argon.sabren.com>
2011-06-29 16:27:06 +0200

    Centered full screen playback of video, ignoring aspect ratio.

--- src/com/elhaso/tutinterruptvideostream/HelloInterruptVideoStream.java
+++ src/com/elhaso/tutinterruptvideostream/HelloInterruptVideoStream.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.WindowManager;
 import android.widget.MediaController;
 
@@ -11,6 +12,7 @@ public class HelloInterruptVideoStream extends Activity
 {
 	private String path = "http://dl.dropbox.com/u/145894/t/rabbits.3gp";
 	private VideoViewCustom video_view_;
+	private int screen_width, screen_height;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -18,13 +20,25 @@ public class HelloInterruptVideoStream extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		getWindow().clearFlags(WindowManager
+			.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+			WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		Display display = getWindowManager().getDefaultDisplay();
+		screen_width = display.getWidth();
+		screen_height = display.getHeight();
+		//screen_width = screen_height = 200;
+
 		video_view_ = (VideoViewCustom)findViewById(R.id.surface_view);
-		video_view_.setDimensions(200, 200);
-		video_view_.getHolder().setFixedSize(200, 200);
+		video_view_.setDimensions(screen_width, screen_height);
+		video_view_.getHolder().setFixedSize(screen_width, screen_height);
 
 		video_view_.setVideoURI(Uri.parse(path));
 		video_view_.setMediaController(new MediaController(this));
 		video_view_.requestFocus();
+		video_view_.start();
 	}
 
 	@Override
@@ -38,19 +52,22 @@ public class HelloInterruptVideoStream extends Activity
 				.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-			*/
-
-			video_view_.setDimensions(200, 200);
-			video_view_.getHolder().setFixedSize(200, 200);
-		} else {
-			/*
-			getWindow().setFlags(
-				WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 				*/
 
-			video_view_.setDimensions(200, 200);
-			video_view_.getHolder().setFixedSize(200, 200);
+			//Log.i("", "Setting dimension to " + screen_height + "x" + screen_width);
+			video_view_.setDimensions(screen_height, screen_width);
+			video_view_.getHolder().setFixedSize(screen_height, screen_width);
+		} else {
+			/*
+			getWindow().clearFlags(WindowManager
+				.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+				*/
+
+			//Log.i("", "Setting dimension to " + screen_width + "x" + screen_height);
+			video_view_.setDimensions(screen_width, screen_height);
+			video_view_.getHolder().setFixedSize(screen_width, screen_height);
 		}
 	}
 }
